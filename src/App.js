bc16a0f166635c6578dc6388fa0a8db695dc50a5
dbushey <polendeuva@gmail.com>
2018-09-17 18:35:47 -0400

    Changing a component dynamically using state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from  './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Dai", age: "35"},
+      {name: "Juju", age: "500"},
+      {name: "Nana", age: "300"}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>I am a React App</h1>
-        <Person name="Dai" age="35"/>
-        <Person name="Juju" age="500">My hobbies: dancing</Person>
-          <Person name="Nana" age="300"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: dancing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
         </div>
         );
   }
