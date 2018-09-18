23e904d2e19653c47e811bbf8c85fa84cd9532b8
dbushey <polendeuva@gmail.com>
2018-09-18 17:47:02 -0400

    Passing method references between components

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log("Was clicked!")
     this.setState({
       persons: [
-        {name: "Daizinha", age: "35"},
+        {name: newName, age: "35"},
         {name: "Juju", age: "500"},
         {name: "Nana", age: "3000000"}
       ]
@@ -26,11 +26,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>I am a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Daizinha!!')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: dancing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Dai!!!")}>My hobbies: dancing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        </div>
+      </div>
         );
   }
 }
