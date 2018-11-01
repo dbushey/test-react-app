e36d90504356eedcd43d6a759a9372973fbe3c68
dbushey <polendeuva@gmail.com>
2018-11-01 16:51:06 -0400

    Add a delete at a click handler to mapped items in a list

--- src/App.js
+++ src/App.js
@@ -13,17 +13,7 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = ( newName ) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState( {
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    } )
-  }
+  
 
   nameChangedHandler = ( event ) => {
     this.setState( {
@@ -33,6 +23,13 @@ class App extends Component {
         { name: 'Stephanie', age: 26 }
       ]
     } )
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.person
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+
   }
 
   togglePersonsHandler = () => {
@@ -54,8 +51,9 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-        {this.state.persons.map(person = () => {
+        {this.state.persons.map((person, index) => {
           return <Person 
+                  click={() => this.deletePersonHandler(index)}
                   name={person.name}
                   age={person.age}/>
         })}
