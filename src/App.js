46f612dd557232c0be766e3f6a64cbfd9edc2c62
dbushey <polendeuva@gmail.com>
2018-11-01 15:49:17 -0400

    Handling dynamic content using a variable

--- src/App.js
+++ src/App.js
@@ -1,77 +1,85 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from  './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      {name: "Dai", age: "35"},
-      {name: "Juju", age: "500"},
-      {name: "Nana", age: "300"}
-    ]
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    //console.log("Was clicked!")
-    this.setState({
+  switchNameHandler = ( newName ) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
       persons: [
-        {name: newName, age: "35"},
-        {name: "Juju", age: "500"},
-        {name: "Nana", age: "3000000"}
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
       ]
-    })
+    } )
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
+  nameChangedHandler = ( event ) => {
+    this.setState( {
       persons: [
-        {name: "Dai", age: "35"},
-        {name: event.target.value, age: "500"},
-        {name: "Nana", age: "3000000"}
-        showPersons: false
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
       ]
-    })
+    } )
   }
 
-  togglePersonHandler = () => {
-    doesShow = this.state.showPersons;
-    this.setState({
-      showPersons = !doesShow
-    })
-
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
   }
 
-  render() {
+  render () {
     const style = {
-      backgroundColor : 'white',
+      backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind( this, 'Max!' )}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
     }
 
     return (
       <div className="App">
-        <h1>I am a React App</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
         <button
           style={style}
-          onClick={() => this.togglePersonHandler('Daizinha!!')}>Switch Name</button>
-          {this.state.showPersons === true ?
-            <div>
-              <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-              <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age} 
-              click={this.switchNameHandler.bind(this, "Dai!!!")} 
-              changed={this.nameChangedHandler}>My hobbies: dancing/>
-              <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}/>
-          </div>}   : null
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
-        );
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
